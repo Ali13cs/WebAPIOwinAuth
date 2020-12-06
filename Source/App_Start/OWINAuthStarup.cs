d2720f6dc0501723ea34a1a3147a2640664a2dbc
mohammad.ali <m.ali13cs@gmail.com>
2020-12-07 01:08:46 +0500

    Added OWIN packages and OWIN Startup class

---
+++ Source/App_Start/OWINAuthStarup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAPIOwinAuth.App_Start.OWINAuthStarup))]
+
+namespace WebAPIOwinAuth.App_Start
+{
+    public class OWINAuthStarup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
